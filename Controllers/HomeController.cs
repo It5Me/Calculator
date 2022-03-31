6662e927d4c432f6a13c734dacdfe60385e06d73
It5Me <56817602+It5Me@users.noreply.github.com>
2022-04-01 01:01:01 +0700

    DONE

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Calculator_62010634.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace Calculator_62010634.Controllers
+{
+    public class HomeController : Controller
+    
+    {
+        [Produces("text/html")]
+        public IActionResult Caculator(string numberinp)
+        {
+            string request = numberinp;
+            string value = new DataTable().Compute(request, null).ToString();
+            string msg = value;
+            return Content(msg, "text/html", Encoding.UTF8);
+        }  
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Calculator_62010634.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
